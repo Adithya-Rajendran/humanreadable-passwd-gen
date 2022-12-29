5056d35aa3182dde1f2b453e9b0535f6248686cd
Adithya Rajendran <adithyaraj@gmail.com>
2022-12-28 20:06:05 -0800

    comment out testing code

--- passwd_gen.py
+++ passwd_gen.py
@@ -1,15 +1,15 @@
 from password_strength import PasswordPolicy
 from password_strength import PasswordStats
 from hashlib import sha1
-from getpass import getpass
+# from getpass import getpass
 import requests
 
 def passwd_test(policy: PasswordPolicy, password: str) -> bool:
     result = policy.test(password)
     if result:
-        print("Weak Password,", result)
-        stats = PasswordStats(password)
-        print(f"Entropy = {stats.entropy_bits} : strength = {stats.strength()}")
+        # print("Weak Password,", result)
+        # stats = PasswordStats(password)
+        # print(f"Entropy = {stats.entropy_bits} : strength = {stats.strength()}")
         return False
 
     passhash = sha1(password.encode("utf-8")).hexdigest().upper()
@@ -17,7 +17,7 @@ def passwd_test(policy: PasswordPolicy, password: str) -> bool:
     response = requests.get(f"https://api.pwnedpasswords.com/range/{first_characters}")
     for header in response.text.splitlines():
         if passhash[6:] in header:
-            print("Password Pwned")
+            # print("Password Pwned")
             return False
     return True
 
@@ -26,4 +26,4 @@ if __name__=='__main__':
         entropybits=66,
         strength=(0.66, 30)
     )
-    print(passwd_test(policy, getpass(prompt = "Please type your password: ", stream = None)))
+    # print(passwd_test(policy, getpass(prompt = "Please type your password: ", stream = None)))
